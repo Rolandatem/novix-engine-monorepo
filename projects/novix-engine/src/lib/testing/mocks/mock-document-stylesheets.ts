c75a54934a48d179cd8923dd9021c899aab1d2d1
Steve <steve.martinez@incstores.com>
2025-09-25 18:12:35 -0700

    forgot to comment one of the new mocking objects.

--- projects/novix-engine/src/lib/testing/mocks/mock-document-stylesheets.ts
+++ projects/novix-engine/src/lib/testing/mocks/mock-document-stylesheets.ts
@@ -1,26 +1,37 @@
+/**
+ * Mocks the 'document.styleSheets' API for unit tests tha rely on CSS class detection.
+ *
+ * This is used to simulate the presence (or absence) of CSS classes in loaded stylesheets,
+ * allowing tests to validate behavior of 'cssClassExists()' and theme fallback logic.
+ *
+ * @param classesToAdd Array of CSS class name (without leading dot) to simulate as preset in the stylesheet.
+ * Each class will be converted to a mock 'CSSStyleRule' with a '.selectorText' like '.theme-name'.
+ *
+ * @param includeInaccessibleStylesheet If 'true', includes a second stylesheet that throws an exception
+ * when attempted to access. This allows coverage of the 'catch' block in 'cssClassExists()' for inaccessible stylesheets.
+ */
 export function mockDocumentStyleSheets(
   classesToAdd: string[],
   includeInaccessibleStylesheet: boolean = false
 ): void {
 
-  //--Cant 'new' up a CSSStyleRule because apparently it's part
-  //--of the browser's internal CSS Object Model and instances
-  //--are created when it parses stylesheets. The constructor
-  //--is blocked to prevent manual instatiation. I wonder why.
+  //--Simulate a stylesheet with accessible CSS rules.
   const accessibleStyleSheet = {
     cssRules: classesToAdd.map(className =>
       Object.setPrototypeOf({
         selectorText: `.${className}`,
         style: {}
-      }, CSSStyleRule.prototype))
+      }, CSSStyleRule.prototype)) //--Required to pass 'instanceof CSSStyleRule' checks.
   }
 
+  //--Somulate a stylesheet that throws an exception when accessed.
   const inaccessibleStyleSheet = {
     get cssRules() {
       throw new Error('not allowed to access.')
     }
   }
 
+  //--Mock document.styleSheets with the mocked stylesheets.
   Object.defineProperty(document, 'styleSheets', {
     configurable: true,
     get: () => includeInaccessibleStylesheet
