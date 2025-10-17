d8cc781161a4016907e2cb9ecc656145371f538d
Steve <steve.martinez@incstores.com>
2025-10-17 12:56:57 -0700

    removed hard-coded left position to by dynamic (prepping for other attached directions).

--- projects/novix-engine/src/lib/components/novix-cabinet/directives/novix-cabinet-tray-directive.ts
+++ projects/novix-engine/src/lib/components/novix-cabinet/directives/novix-cabinet-tray-directive.ts
@@ -1,21 +1,57 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Directive, ElementRef, inject, input, OnDestroy, PLATFORM_ID, signal, TemplateRef } from '@angular/core';
+import { AfterViewInit, computed, Directive, inject, input, OnDestroy, PLATFORM_ID, signal, TemplateRef } from '@angular/core';
 
 @Directive({
   selector: '[novix-cabinet-tray]'
 })
 
-export class NovixCabinetTrayDirective implements OnDestroy {
+export class NovixCabinetTrayDirective implements AfterViewInit, OnDestroy {
   private _isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
   private _boundOutsideClickEvent = this.handleOutsideClick.bind(this);
   private _trayEl?: HTMLElement;
   private _handleEl?: HTMLElement;
+  private _trayHandleSize = signal<string>('0px');
 
   //==== PUBLIC PROPERTIES =====================================================
   public handleText = input<string>('');
   public trayId = input<string>('');
   public isOpen = signal<boolean>(false);
+  public traySize = input<string>('');
+  public attachDirection = input<'left' | 'right' | 'top' | 'bottom'>('left');
+  public trayContainerSize = signal<string>('');
+  public isVertical = computed(() => ['top', 'bottom'].includes(this.attachDirection()));
+  public calculatedWidth = computed(() => {
+    return this.isVertical() ? null : this.trayContainerSize();
+  });
+  public calculatedHeight = computed(() => {
+    return this.isVertical() ? this.trayContainerSize() : null;
+  });
+  public calculatedLeftPosition = computed(() => this.commonPositionCalculation('left'));
+  public calculatedHandleLeftPosition = computed(() => {
+    if (!this.isOpen()) { return '0px'; }
+    else { return this.trayContainerSize(); }
+  });
+  public trayClosedOffset = computed(() => `calc(-1 * (${this.trayContainerSize()} - ${this._trayHandleSize()}))`);
   public readonly templateRef = inject<TemplateRef<unknown>>(TemplateRef);
+
+  private commonPositionCalculation(direction: 'left' | 'right' | 'top' | 'bottom'): string | null {
+    const attached = this.attachDirection();
+
+    const isSameDirection = attached === direction;
+    const isOpposite = (attached === 'left' && direction === 'right') ||
+                       (attached === 'right' && direction === 'left') ||
+                       (attached === 'top' && direction === 'bottom') ||
+                       (attached === 'bottom' && direction === 'top');
+
+    if (isSameDirection && !this.isOpen())
+    { return this.trayClosedOffset(); }
+    else if (isSameDirection && this.isOpen())
+    { return null; }
+
+    if (isOpposite) { return 'auto'; }
+
+    return '0px';
+  }
 
   public open(): void {
     this.isOpen.set(true);
@@ -41,6 +77,18 @@ export class NovixCabinetTrayDirective implements OnDestroy {
     }
   }
 
+  public ngAfterViewInit(): void {
+    //--Determine tray size by using specified value, otherwise default.
+    this.trayContainerSize.set(this.traySize() !== ''
+      ? this.traySize()
+      : ['left','right'].includes(this.attachDirection()) ? '300px' : '500px');
+
+    //--Measure tray handle size based on direction.
+    this._trayHandleSize.set(this.isVertical()
+      ? this.calculateHandleHeight()
+      : this.calculateHandleWidth());
+  }
+
   public ngOnDestroy(): void {
     if (this._isBrowser) {
       document.removeEventListener('click', this._boundOutsideClickEvent, true);
@@ -62,5 +110,15 @@ export class NovixCabinetTrayDirective implements OnDestroy {
       //--So only if clicked outside both tray or handle, close it.
       this.close();
     }
+  }
+
+  public calculateHandleHeight() {
+    const height = this._handleEl?.offsetHeight ?? 0;
+    return `${height}px`;
+  }
+
+  public calculateHandleWidth() {
+    const width = this._handleEl?.offsetWidth ?? 0;
+    return `${width}px`;
   }
 }
